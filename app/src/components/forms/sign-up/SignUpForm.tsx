9a9e4143e16053331fdb34857b6d1020f2332f2b
Erica Carvalho <ericamcar28@gmail.com>
2021-08-30 11:25:31 +0100

    fix border color

--- app/src/components/forms/sign-up/SignUpForm.tsx
+++ app/src/components/forms/sign-up/SignUpForm.tsx
@@ -26,7 +26,7 @@ const SignUpForm: FC = () => {
 		<form 
 			noValidate
 			onSubmit={handleSubmit(onSubmit)} 
-			className="grid gap-x-4 gap-y-3"
+			className="grid gap-x-4 gap-y-3 sm:block"
 			style={{gridTemplate: `
 				"firstName lastName"
 				"email email"
@@ -42,25 +42,25 @@ const SignUpForm: FC = () => {
 
 			<fieldset style={{gridArea: `lastName`}}>
 				<label  htmlFor="last-name">Last name</label>
-				<input className={`block ${errors.lastName && `border-red-700`}`} {...register(`lastName`)} id="last-name" type="text" />
+				<input className={`block ${errors.lastName && `border-danger`}`} {...register(`lastName`)} id="last-name" type="text" />
 				<p className="text-danger text-sm">{errors.lastName?.message}</p>
 			</fieldset>
 
 			<fieldset style={{gridArea: `email`}}>
 				<label htmlFor="email">Email</label>
-				<input className={`w-full block ${errors.email && `border-red-700`}`} {...register(`email`)} id="email" type="email" />
+				<input className={`w-full block ${errors.email && `border-danger`}`} {...register(`email`)} id="email" type="email" />
 				<p className="text-danger text-sm">{errors.email?.message}</p>
 			</fieldset>
 
 			<fieldset style={{gridArea: `password`}}>
 				<label htmlFor="password">Password</label>
-				<input className={`block ${errors.password && `border-red-700`}`} {...register(`password`)} id="password" type="text" />
+				<input className={`block ${errors.password && `border-danger`}`} {...register(`password`)} id="password" type="text" />
 				<p className="text-danger text-sm">{errors.password?.message}</p>
 			</fieldset>
 
 			<fieldset style={{gridArea: `confirmPassword`}}>
 				<label htmlFor="confirm-password">Confirm password</label>
-				<input className={`block ${errors.confirmPassword && `border-red-700`}`} {...register(`confirmPassword`)} id="confirm-password" type="text" />
+				<input className={`block ${errors.confirmPassword && `border-danger`}`} {...register(`confirmPassword`)} id="confirm-password" type="text" />
 				<p className="text-danger text-sm">{errors.confirmPassword?.message}</p>
 			</fieldset>
 
