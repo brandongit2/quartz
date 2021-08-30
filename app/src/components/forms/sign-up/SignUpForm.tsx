c0ac1a871e2546b95cf5c377e116b305cc18ba23
Erica Carvalho <ericamcar28@gmail.com>
2021-08-30 17:01:38 +0100

    remove confirm password

--- app/src/components/forms/sign-up/SignUpForm.tsx
+++ app/src/components/forms/sign-up/SignUpForm.tsx
@@ -10,7 +10,6 @@ type FormValues = {
 	lastName: string,
 	email: string,
 	password: string,
-	confirmPassword: string
 }
 
 const SignUpForm: FC = () => {
@@ -30,7 +29,7 @@ const SignUpForm: FC = () => {
 			style={{gridTemplate: `
 				"firstName lastName"
 				"email email"
-				"password confirmPassword"
+				"password password"
 				"button button"
 			`}}
 		>
@@ -54,14 +53,8 @@ const SignUpForm: FC = () => {
 
 			<fieldset style={{gridArea: `password`}} className="m-2">
 				<label htmlFor="password">Password</label>
-				<input className={`block ${errors.password && `border-danger`}`} {...register(`password`)} id="password" type="text" />
+				<input className={`sm:w-full block ${errors.password && `border-danger`}`} {...register(`password`)} id="password" type="text" />
 				<p className="text-danger text-sm">{errors.password?.message}</p>
-			</fieldset>
-
-			<fieldset style={{gridArea: `confirmPassword`}} className="m-2">
-				<label htmlFor="confirm-password">Confirm password</label>
-				<input className={`block ${errors.confirmPassword && `border-danger`}`} {...register(`confirmPassword`)} id="confirm-password" type="text" />
-				<p className="text-danger text-sm">{errors.confirmPassword?.message}</p>
 			</fieldset>
 
 			<input style={{gridArea: `button`}} className="button m-2" type="submit" />
--- app/src/components/forms/sign-up/SignUpFormSchema.ts
+++ app/src/components/forms/sign-up/SignUpFormSchema.ts
@@ -5,7 +5,6 @@ const SignUpFormSchema = yup.object().shape({
 	lastName: yup.string().required(`Please enter your last name`),
 	email: yup.string().required(`Please enter your email`).email(`Please enter a valid email`),
 	password: yup.string().required(`Please enter a password`).min(8, `Password must be at least 8 characters long`),
-	confirmPassword: yup.string().required(`Please confirm your password`).oneOf([yup.ref(`password`)], `Passwords must match`)
 })
 
 export default SignUpFormSchema
